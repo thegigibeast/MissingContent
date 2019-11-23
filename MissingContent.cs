c8d46b3b4c3a09403a1da8ca7078ff9ee8a10c90
thegigibeast <thegigibeast@gmail.com>
2019-11-23 09:51:04 -0500

    Mod Helpers integration (https://forums.terraria.org/index.php?threads/mod-helpers.63670/)

--- MissingContent.cs
+++ MissingContent.cs
@@ -2,10 +2,23 @@ using Terraria.ModLoader;
 
 namespace MissingContent
 {
-	public class MissingContent : Mod
-	{
-		public MissingContent()
-		{
-		}
-	}
+    public class MissingContent : Mod
+    {
+        #region Public Constructors
+
+        public MissingContent()
+        {
+        }
+
+        #endregion Public Constructors
+
+        #region Public Properties
+
+        // Mod Helpers integration
+        public static string GithubProjectName => "MissingContent";
+
+        public static string GithubUserName => "thegigibeast";
+
+        #endregion Public Properties
+    }
 }
